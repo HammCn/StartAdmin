a8412f7fe5cac709337c250d6cc135adf01aaf19
Hamm <majhamm@qq.com>
2021-01-28 23:01:03 +0800

    fix: 修复一个上传漏洞

--- app/api/controller/Attach.php
+++ app/api/controller/Attach.php
@@ -37,7 +37,7 @@ class Attach extends BaseController
         try {
             $file = request()->file('file');
             try {
-                validate(['file' => 'filesize:' . config("startadmin.upload_max_image") . '|fileExt:php,' . config("startadmin.upload_image_type")])
+                validate(['file' => 'filesize:' . config("startadmin.upload_max_image") . '|fileExt:' . config("startadmin.upload_image_type")])
                     ->check(['file' => $file]);
                 $saveName = Filesystem::putFile('image', $file);
                 $attach_data = array(
@@ -73,7 +73,7 @@ class Attach extends BaseController
         try {
             $file = request()->file('file');
             try {
-                validate(['file' => 'filesize:' . config("startadmin.upload_max_file") . '|fileExt:php,' . config("startadmin.upload_file_type")])
+                validate(['file' => 'filesize:' . config("startadmin.upload_max_file") . '|fileExt:' . config("startadmin.upload_file_type")])
                     ->check(['file' => $file]);
                 $saveName = Filesystem::putFile('normal', $file);
                 $attach_data = array(
